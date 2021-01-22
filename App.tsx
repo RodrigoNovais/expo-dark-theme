e586459454eda66a5944c7059430c4b6f13a8c48
Rodrigo Tolentino de Novais <roo.novais@gmail.com>
2021-01-22 18:36:22 -0300

    Setup project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Fragment } from 'react'
+import { StatusBar } from 'expo-status-bar'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App: React.FC = () => (
+    <Fragment>
+        <StatusBar style='auto' />
+    </Fragment>
+)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
